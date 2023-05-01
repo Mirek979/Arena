5afc504ee29dab8dc0c0e86af885399b13aab550
User <User@MIRV-PC>
2023-05-01 08:37:47 +0200

    Add project files.

---
+++ ConsoleApp3/Kostka.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arena
+{
+    public class Kostka
+    {
+        private Random random;
+        private int pocetSten;
+    }
+}
